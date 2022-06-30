af6161f36c440c64fc2cb8f11f2f00c6b3f3cc46
Benceroni <90892977+Benceroni@users.noreply.github.com>
2022-06-30 13:09:31 -0600

    Adding cycles base to work from

---
+++ cse210-05-cycles-main/cycle/game/casting/score.py
@@ -0,0 +1,38 @@
+from game.casting.actor import Actor
+
+
+class Score(Actor):
+    """
+    A record of points made or lost. 
+    
+    The responsibility of Score is to keep track of the points the player has earned by beating the other player.
+    It contains methods for adding and getting points. Client should use get_text() to get a string 
+    representation of the points earned.
+
+    Attributes:
+        _points (int): The points earned in the game.
+    """
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+        self.add_points(0)
+
+    def add_points(self, points):
+        """Adds the given points to the score's total points.
+        
+        Args:
+            points (int): The points to add.
+        """
+        self._points += points
+        self.set_text(f"Score: {self._points}")
+
+    def get_points(self):
+        """Unwritten in original example. 
+        This returns the value of the points.
+        
+        Args:
+            None.
+        Returns:
+            (int)
+        """
+        return self._points
